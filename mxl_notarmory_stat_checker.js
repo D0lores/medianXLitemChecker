2a7d7ef9d49c5594fd03d8ebca5d13dbca0dbf0b
D0lores <56717078+D0lores@users.noreply.github.com>
2021-08-06 14:47:04 +0800

    Update mxl_notarmory_stat_checker.js

--- mxl_notarmory_stat_checker.js
+++ mxl_notarmory_stat_checker.js
@@ -26,9 +26,12 @@ function findDiff(str1, str2){
         if (str1[i] == '#') {
             return parseInt(str2.substr(j, str2.length))
         }
-        if(str1[i])!="^")
+        if(str1[i])=='^')
+            ++i;
+        else
             ++j;
-        ++i;
+            ++i;
+        
         
     }
   return diff;
